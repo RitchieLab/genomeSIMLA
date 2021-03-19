65237a560b511abb4e87d5b4c013ebfe588fb120
Alexander Frase <atfrase@pennmedicine.upenn.edu>
2021-03-19 10:40:15 -0400

    fixed compiler symbol conflict by renaming M -> MM

--- trunk/src/simla/random.cpp
+++ trunk/src/simla/random.cpp
@@ -443,16 +443,16 @@ double Random::ranb()
         if (mti == NN+1)   /* if sgenrand() has not been called, */
             sgenrand(seed); /* a default initial seed is used   */
 
-        for (kk=0;kk<NN-M;kk++) {
+        for (kk=0;kk<NN-MM;kk++) {
             y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1];
+            mt[kk] = mt[kk+MM] ^ (y >> 1) ^ mag01[y & 0x1];
         }
         for (;kk<NN-1;kk++) {
             y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(M-NN)] ^ (y >> 1) ^ mag01[y & 0x1];
+            mt[kk] = mt[kk+(MM-NN)] ^ (y >> 1) ^ mag01[y & 0x1];
         }
         y = (mt[NN-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[NN-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1];
+        mt[NN-1] = mt[MM-1] ^ (y >> 1) ^ mag01[y & 0x1];
 
         mti = 0;
     }
--- trunk/src/simla/random.h
+++ trunk/src/simla/random.h
@@ -44,7 +44,7 @@ typedef unsigned int uint32;
 /* Period parameters */  
 #define NN 624
 
-#define M 397
+#define MM 397
 #define MATRIX_A 0x9908b0df   /* constant vector a */
 #define UPPER_MASK 0x80000000 /* most significant w-r bits */
 #define LOWER_MASK 0x7fffffff /* least significant r bits */
