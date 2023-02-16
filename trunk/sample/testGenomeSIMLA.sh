5aae98ae0b2cf931ef55ff050a86b9c5c3ddc45b
XueLi <30300080+XueqiongLi@users.noreply.github.com>
2023-02-16 11:38:00 -0500

    Add sample for users to test

---
+++ trunk/sample/testGenomeSIMLA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############################################################################
+#                 genomeSIMLA example test commands                           #
+###############################################################################
+
+#module load genomeSIMLA/1.2.0
+
+# generate dataset
+genomeSIMLA metaDimSimTest.sim
+
+# analyze based on generated dataset
+genomeSIMLA metaDimSimTest.sim -l 450
